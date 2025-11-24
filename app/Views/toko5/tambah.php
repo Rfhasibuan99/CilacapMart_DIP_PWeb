f1d4ef146964af377b4e22d0c63f9f3fc4110569
Rfhasibuan99 <riofh99.stu@pnc.ac.id>
2025-11-24 15:19:42 +0700

    ini index sama tambah nyaa

--- app/Views/toko5/tambah.php
+++ app/Views/toko5/tambah.php
@@ -5,7 +5,11 @@
     <div class="col">
         <h3 class="mt-2">Form Tambah Barang</h3>
 
-        <form action="/toko5/simpan" method="post" class="mt-4" enctype="multipart/form-data">
+        <?php if(session()->getFlashdata('error')): ?>
+            <div class="alert alert-danger"><?= session()->getFlashdata('error'); ?></div>
+        <?php endif; ?>
+
+        <form action="/toko1/simpan" method="post" class="mt-4" enctype="multipart/form-data">
             <?= csrf_field(); ?>
 
             <!-- Nama Barang -->
@@ -14,10 +18,13 @@
                 <div class="col-sm-4">
                     <input 
                         type="text" 
-                        class="form-control"
+                        class="form-control <?= (session('errors.nama_barang')) ? 'is-invalid' : ''; ?>"
                         name="nama_barang"
                         value="<?= old('nama_barang'); ?>"
                         required>
+                    <div class="invalid-feedback">
+                        <?= session('errors.nama_barang') ?>
+                    </div>
                 </div>
             </div>
 
@@ -27,23 +34,58 @@
                 <div class="col-sm-4">
                     <input 
                         type="text" 
-                        class="form-control"
+                        class="form-control <?= (session('errors.jenis_barang')) ? 'is-invalid' : ''; ?>"
                         name="jenis_barang"
                         value="<?= old('jenis_barang'); ?>"
                         required>
+                    <div class="invalid-feedback">
+                        <?= session('errors.jenis_barang') ?>
+                    </div>
                 </div>
             </div>
 
             <!-- Harga Barang -->
             <div class="form-group row mb-3">
-                <label class="col-sm-2 col-form-label">Harga Barang</label>
+                <label class="col-sm-2 col-form-label">Harga Beli</label>
                 <div class="col-sm-4">
                     <input 
                         type="number" 
-                        class="form-control"
-                        name="harga_barang"
-                        value="<?= old('harga_barang'); ?>"
+                        class="form-control <?= (session('errors.harga_beli')) ? 'is-invalid' : ''; ?>"
+                        name="harga_beli"
+                        value="<?= old('harga_beli'); ?>"
                         required>
+                    <div class="invalid-feedback">
+                        <?= session('errors.harga_beli') ?>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row mb-3">
+                <label class="col-sm-2 col-form-label">Harga Jual</label>
+                <div class="col-sm-4">
+                    <input 
+                        type="number" 
+                        class="form-control <?= (session('errors.harga_jual')) ? 'is-invalid' : ''; ?>"
+                        name="harga_jual"
+                        value="<?= old('harga_jual'); ?>"
+                        required>
+                    <div class="invalid-feedback">
+                        <?= session('errors.harga_jual') ?>
+                    </div>
+                </div>
+            </div>
+            <!-- Stok Barang -->
+            <div class="form-group row mb-3">
+                <label class="col-sm-2 col-form-label">Stok Barang</label>
+                <div class="col-sm-4">
+                    <input 
+                        type="number" 
+                        class="form-control <?= (session('errors.stok')) ? 'is-invalid' : ''; ?>"
+                        name="stok"
+                        value="<?= old('stok'); ?>"
+                        required>
+                    <div class="invalid-feedback">
+                        <?= session('errors.stok') ?>
+                    </div>
                 </div>
             </div>
 
@@ -52,10 +94,13 @@
                 <label class="col-sm-2 col-form-label">Deskripsi Barang</label>
                 <div class="col-sm-4">
                     <textarea 
-                        class="form-control"
+                        class="form-control <?= (session('errors.deskripsi')) ? 'is-invalid' : ''; ?>"
                         name="deskripsi"
                         rows="3"
                         required><?= old('deskripsi'); ?></textarea>
+                    <div class="invalid-feedback">
+                        <?= session('errors.deskripsi') ?>
+                    </div>
                 </div>
             </div>
 
@@ -65,9 +110,12 @@
                 <div class="col-sm-4">
                     <input 
                         type="file" 
-                        class="form-control"
+                        class="form-control <?= (session('errors.gambar')) ? 'is-invalid' : ''; ?>"
                         name="gambar"
                         required>
+                    <div class="invalid-feedback">
+                        <?= session('errors.gambar') ?>
+                    </div>
                 </div>
             </div>
 
